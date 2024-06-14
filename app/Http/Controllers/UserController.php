51c37ab05c313a9d26bb777ff67ebc318ed9a862
Andre <andre.souza@sp.senai.br>
2024-06-14 17:17:45 -0300

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Illuminate\Facades\Hash;
+use Illuminate\Facades\Auth;
+use Illuminate\Facades\Validator;
+
 
 class UserController extends Controller
 {
