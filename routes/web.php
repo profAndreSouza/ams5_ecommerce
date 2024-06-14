8f5439d3a4c160bc22b4faf761bfc805c092b186
Andre <andre.souza@sp.senai.br>
2024-06-14 17:54:33 -0300

    cloned

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login');
@@ -10,6 +11,9 @@ Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::get('/register', [UserController::class, 'showFormRegister']);
 Route::post('/register', [UserController::class, 'register'])->name('register');
+
+
+Route::get('/products', [ProductController::class, 'listProducts'])->name('listProducts');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [UserController::class, 'profile']);
