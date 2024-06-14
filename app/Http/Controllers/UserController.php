8d042c6b5964466cab3104c12e9ad6e9658b472f
Andre <andre.souza@sp.senai.br>
2024-06-14 17:02:50 -0300

    merge version

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -11,7 +15,25 @@ class UserController extends Controller
     }
 
     function register(Request $request) {
-        return "Enviou form";
+        // Validação dos dados do formulário de registro
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Criação de um novo usuário
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Autentica o usuário recém-registrado
+        Auth::login($user);
+
+        // Redireciona para a página inicial com uma mensagem de sucesso
+        return redirect('/')->with('success', 'Registro realizado com sucesso.');
     }
 
     function profile(Request $request) {
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,16 +10,19 @@
     <form action="{{ route('register') }}" method="post">
         @csrf
         <label for="name">Nome</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name" value="{{ old('name') }}">
+        @error('name') {{ $message }} @enderror
         <br />
         <label for="email">E-mail</label>
-        <input type="text" name="email" id="email">
+        <input type="text" name="email" id="email" value="{{ old('email') }}">
+        @error('email') {{ $message }} @enderror
         <br />
         <label for="password">Senha</label>
         <input type="password" name="password" id="password">
+        @error('password') {{ $message }} @enderror
         <br />
         <label for="confirmation_password">Confirme a senha</label>
-        <input type="password" name="confirmation_password" id="confirmation_password">
+        <input type="password" name="password_confirmation" id="password_confirmation">
         <br />
         <input type="submit" value="Registrar">
     </form>
